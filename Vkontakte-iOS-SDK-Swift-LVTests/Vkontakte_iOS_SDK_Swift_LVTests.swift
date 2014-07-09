45b79a1b4d9740c7d2855f924abec2c74077f5aa
AndrewShmig <andrewshmig@yandex.ru>
2014-07-09 14:51:17 +0400

    Initial Commit

---
+++ Vkontakte-iOS-SDK-Swift-LVTests/Vkontakte_iOS_SDK_Swift_LVTests.swift
@@ -0,0 +1,36 @@
+//
+//  Vkontakte_iOS_SDK_Swift_LVTests.swift
+//  Vkontakte-iOS-SDK-Swift-LVTests
+//
+//  Created by AndrewShmig on 09/07/14.
+//  Copyright (c) 2014 Non Atomic Games Inc. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Vkontakte_iOS_SDK_Swift_LVTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
