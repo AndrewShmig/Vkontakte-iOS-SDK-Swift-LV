5e26bb87231f0f529b5de8d77cf0ca2f8f83cefc
AndrewShmig <andrewshmig@yandex.ru>
2014-07-09 15:11:14 +0400

    Добавлены основные файлы ВКонтакте SDK

---
+++ Vkontakte-iOS-SDK-Swift-LV/VkStorage.swift
@@ -0,0 +1,9 @@
+//
+//  VkStorage.swift
+//  Vkontakte-iOS-SDK-Swift-LV
+//
+//  Created by AndrewShmig on 09/07/14.
+//  Copyright (c) 2014 Non Atomic Games Inc. All rights reserved.
+//
+
+import Foundation
